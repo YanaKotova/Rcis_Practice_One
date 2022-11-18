5e593d16665a542c4fa6a549a556ba1e5ea17706
YanaKotova <117864108+YanaKotova@users.noreply.github.com>
2022-11-18 21:04:20 +0700

    Add files via upload
    practice3

--- practice_3/bin/practice_3.dart
+++ practice_3/bin/practice_3.dart
@@ -3,35 +3,35 @@ import 'dart:math';
 import 'dart:async';
 
 void main() {
-  //задание 3
-  try {
-    File f = File('area.txt');
+//задание 3
+//   try {
+//     File f = File('area.txt');
 
-    dynamic values = (f.readAsStringSync());
-    List<String> stringHeight = values.split(",");
-    List<int> height = stringHeight.map(int.parse).toList();
+//     dynamic values = (f.readAsStringSync());
+//     List<String> stringHeight = values.split(",");
+//     List<int> height = stringHeight.map(int.parse).toList();
 
-    int left = 0;
-    int right = height.length - 1;
-    int max = 0;
+//     int left = 0;
+//     int right = height.length - 1;
+//     int max = 0;
 
-    while (left != right) {
-      int distance = (left - right).abs();
-      int area = 0;
-      if (height[left] < height[right]) {
-        area = height[left] * distance;
-        left++;
-      } else {
-        area = height[right] * distance;
-        right--;
-      }
+//     while (left != right) {
+//       int distance = (left - right).abs();
+//       int area = 0;
+//       if (height[left] < height[right]) {
+//         area = height[left] * distance;
+//         left++;
+//       } else {
+//         area = height[right] * distance;
+//         right--;
+//       }
 
-      if (area > max) {
-        max = area;
-      }
-    }
-    print(max);
-  } catch (e) {
-    print("Возникла ошибка. Возможно, файл недоступен. \nПовторите попытку.");
-  }
-}
+//       if (area > max) {
+//         max = area;
+//       }
+//     }
+//     print(max);
+//   } catch (e) {
+//     print("Возникла ошибка. Возможно, файл недоступен. \nПовторите попытку.");
+//   }
+// }
